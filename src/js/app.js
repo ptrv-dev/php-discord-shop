fd3e517ecb16c6870cf8b25458da21478bf9bf1a
ptrv-dev <petrov99119@gmail.com>
2023-02-27 14:41:09 +0200

    Little styles fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ const headerMenuBtn = document.querySelector(".header__menu");
 const headerNav = document.querySelector(".header__nav");
 
 headerMenuBtn.addEventListener("click", () => {
-  console.log(123);
   if (headerMenuBtn.classList.contains("header__menu_active")) {
     headerMenuBtn.classList.remove("header__menu_active");
     headerNav.classList.remove("header__nav_active");

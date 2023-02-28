25bd87bfacdc50d86aaa197ca20b5056e8c6581f
ptrv-dev <petrov99119@gmail.com>
2023-02-28 19:02:53 +0200

    Order Styles fix

--- src/order/index.php
+++ src/order/index.php
@@ -62,11 +62,12 @@ if (!empty($_POST)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Discrod-Shop &mdash; The best place to buy discord accounts</title>
+    <title>Make order | Discord-Shop &mdash; The best place to buy discord accounts</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/main.min.css">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
 </head>
 
 <body>

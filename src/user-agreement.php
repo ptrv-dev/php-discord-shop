7dc0c2c0d7e82588a0cf55a0ddd4b3c55b98b54b
ptrv-dev <petrov99119@gmail.com>
2023-02-28 13:23:58 +0200

    User Agreement Page

---
+++ src/user-agreement.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Agreement | Discrod-Shop &mdash; The best place to buy discord accounts</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/main.min.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <?php require_once('./includes/navbar.php') ?>
+        <div class="user-agreement">
+            <div class="circle-decor"></div>
+            <div class="circle-decor"></div>
+            <div class="circle-decor circle-decor_light"></div>
+            <div class="circle-decor circle-decor_light"></div>
+            <div class="container user-agreement__container">
+                <h1>User Agreement</h1>
+                <div class="user-agreement__body">
+                    <p>Read the shop regulations before you buy.</p>
+                    <p>1. Accepted goods cannot be returned.</p>
+                    <p>2. The warranty on all goods is 1 hour. (After this time, the product cannot be replaced).</p>
+                    <p>3. The warranty covers the validity of the account, as well as the conformity of the description of the goods.</p>
+                    <p>4. The shop is not responsible for the account activity.</p>
+                    <p>5. After changing the password - the account can not be replaced.</p>
+                    <p>6. The seller does not advise on how to use the accounts. Buy an account only if you know how to use it. Refunds in case of purchase and not knowing how to use it will not be.</p>
+                    <p>7. Shop technical support response time and solve all problems/claims up to 24 hours. (Depends on time of day. On holidays / weekends, the waiting time may be extended).</p>
+                    <p>8. Buying items in our shop, you agree to record the time of purchase, the time of the transfer of funds, the moment of downloading and checking the product on video so that in case of problems with access to the account, we were able to quickly give you a replacement or refund. The video is the only proof on your part that the data at the time of purchase was in fact incorrect. This can be done with Bandicam software. The video must be recorded in full screen, with no freeze frame and with the mouse cursor visible. Otherwise no replacement will be done! If you do not have the opportunity to record the video, you can request a pre-check of the product from the retailer using the contacts listed on the website. Any installation will be withdrawn in favour of the shop.</p>
+                    <p>9. When you purchase an item from the shop for more than $10 - record a video to quickly resolve your issue.</p>
+                    <p>10. If through your fault accounts became invalid after the purchase, such accounts are not subject to replacement.</p>
+                    <p>Payment via Crypto-currency comes with a commission. If you have not paid the full amount, the goods will not be issued automatically, and you will have to contact the shop's support team.</p>
+                    <p>12. if the nonvalidation exceeds 10% the shop has the right to request a video from the moment you clicked the "Buy" button in the shop. Any installation will be withdrawn in favour of the shop.</p>
+                    <p>13. The shop is not responsible for any actions on the sold accounts.</p>
+                    <p>14. In the event of loss of access or blocking in the purchased valid account(s), replacement will not be made.</p>
+                    <p>15. After having purchased multiple accounts, you should first of all verify all of the purchased accounts for validity, and only then can you perform any actions on the account.</p>
+                    <p>16. The buyer has no right to demand a refund if the purchased item cannot be dealt with.</p>
+                    <p>17. Accounts are not refundable, only replaceable, subject to the other rules.</p>
+                    <p>18. Mail account that comes with the account from twitter can be blocked - not working at the time of sale, the responsibility for non-working email shop is not responsible, the guarantee only on the validity of the account.</p>
+                    <p>19. The account is only guaranteed for AUTH_TOKEN validity.</p>
+                    <p>20. For activities performed with our product is not responsible.</p>
+                    <p>21. Use of obscene words may cause refusal of service.</p>
+                    <p>22. Rules can be extended without notice.</p>
+                    <p>23. Ignorance of the terms does not release you from your responsibility.</p>
+                    <p>By purchasing any product, you agree that you have read these terms in full and agree to comply with them!</p>
+                </div>
+            </div>
+        </div>
+        <?php require_once('./includes/footer.php') ?>
+    </div>
+</body>
+
+</html>

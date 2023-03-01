9f15cc40d5144b35b6fc15377c30a451cb53963c
ptrv-dev <petrov99119@gmail.com>
2023-03-01 10:55:45 +0200

    gitignore fix

--- dist/vendor/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$MYSQL_HOST = 'localhost';
-$MYSQL_DB = 'discord-shop';
-
-$MYSQL_USER = 'root';
-$MYSQL_PASS = '';
-
-$pdo = new PDO("mysql:host=$MYSQL_HOST;dbname=$MYSQL_DB", $MYSQL_USER, $MYSQL_PASS);
